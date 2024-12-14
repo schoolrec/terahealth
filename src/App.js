1376e26599d9a28398fc52bbf93263e697546fc9
saif saeed <ssr.on.2010@gmail.com>
2024-12-14 17:23:25 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+import React from 'react';
+import Header from './Header';  
+import Landing from './Landing';  
+import './App.css';  
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <Landing />
+    </div>
+  );
+};
+
+export default App;
